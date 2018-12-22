51fad0f1e59afd7c53029fa7dec3ab2b731a4246
Brian Tracy <brian.tracy33@gmail.com>
2018-12-22 14:48:08 -0800

    got networking working, can make connection to local server

--- client.c
+++ client.c
@@ -2,10 +2,77 @@
 #include <unistd.h>
 #include <curses.h>
 
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
+
+int get_socket(const char *server, const char *port) {
+    // setup for getaddrinfo
+    int sock;
+    struct addrinfo hints;
+    struct addrinfo *result;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    int err;
+    if ((err = getaddrinfo(server, port, &hints, &result)) != 0) {
+        fprintf(stderr, "Error in getaddrinfo: %s\n", gai_strerror(err));
+        return -1;
+    }
+
+    // find the right interface
+    struct addrinfo *res;
+    for (res = result; res != NULL; res = res->ai_next) {
+        if ((sock = socket(res->ai_family, res->ai_socktype,
+                           res->ai_protocol)) < 0) {
+            continue;
+        }
+        if (connect(sock, res->ai_addr, res->ai_addrlen) >= 0) {
+            break;
+        }
+        close(sock);
+    }
+
+    freeaddrinfo(result);
+
+    if (res == NULL) {
+        fprintf(stderr, "Failed to connect to '%s'!\n", server);
+        return -1;
+    }
+
+    return sock;
+}
 
 int main(int argc, char *argv[]) {
     
+    int s = get_socket(argv[1], argv[2]);
+    if (s == -1) {
+     
+       printf("could not get socket\n");
+        return 0;
+    }
+
+    FILE *cxn = fdopen(s, "rw");
+    fputs("abcdef", cxn);
+    
+    return 0;
+}
+
+void x() {
+
     initscr();
     noecho();
     curs_set(FALSE);
@@ -40,5 +107,4 @@ sleep(1);
    // pause();
     endwin();
     printf("done\n");
-    return 0;
 }
